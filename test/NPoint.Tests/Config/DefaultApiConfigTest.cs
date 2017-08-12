a307f1f378d564058238361db64cce1717f5395e
Justin Zhong <email@justinzhong.com>
2017-08-12 12:12:52 +1000

    Add test for DefaultApiConfig

---
+++ test/NPoint.Tests/Config/DefaultApiConfigTest.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NPoint.Config;
+using NPoint.Serialization;
+using Xunit;
+
+namespace NPoint.Tests.Config
+{
+    public class DefaultApiConfigTest
+    {
+        [Fact]
+        public void ShouldReturnJsonResponseConverter()
+        {
+            // Arrange
+            var expected = typeof(JsonResponseConverter);
+
+            // Act
+            var sut = new DefaultApiConfig();
+            var converter = sut.ResponseConverter;
+            var actual = converter.GetType();
+
+            // Assert
+            converter.Should().NotBeNull();
+            actual.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
